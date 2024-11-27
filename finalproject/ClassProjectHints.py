defd1dd9482aae48382c09d883c5215e90026203
Sam <drpong2@gmail.com>
2024-11-27 03:01:44 -0600

    add classhints
    Copying ClassProjectHints.,py to folder for conciseness.

---
+++ finalproject/ClassProjectHints.py
@@ -0,0 +1,59 @@
+
+
+def auth():
+	print('='*22)
+	print("=CLASS LOGIN AND MENU=")
+	print('='*22)
+
+	print("press 1 to login, 2 to create a login, 'q' to quit: ")
+'''
+The below is Pseudo-code. It should not be copied directly, but used as a guide for writing your own program.
+
+username = input("Please enter your username: ")
+
+password = input("Please enter your password: ")
+
+
+itemlist = ["item 1", "item 2", "item 3"]
+
+userauth = {'steve': 'opensesame'}
+
+def checkpassword(username, password):
+	tries = 3
+	while tries > 0:
+		if userauth[username] == password
+			login
+		else:
+			tries -= 1
+			password=input(f"Password incorrect! {tries} remaining\nEnter password: ")
+
+#Using functions for flow control of your app is recommended
+#the following functions are ones I was planning on using:
+#	getroles(), listapps(), login()
+#the below may give some insight on how to use them.
+#Remember, the goal of this exercise is "flow control" - How do you give users options and manage where they are within your program?
+
+
+Functions also make your code easier to test and debug. When the bulk
+of your program’s work is done by a set of functions, each of which has a
+specific job, it’s much easier to test and maintain the code you’ve written.
+You can write a separate program that calls each function and tests whether
+each function works in all the situations it may encounter. When you do
+this, you can be confident that your functions will work properly each time
+you call them.
+
+Step 1:
+Try loadfile()
+except FileNotFoundError
+print "No user exists! please create a user!"
+
+
+
+'''
+
+
+
+
+
+
+
